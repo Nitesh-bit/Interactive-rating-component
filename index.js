2afc92fa28e6cae24999d17c9ca2a968b4941677
Nitesh <chaudharynitesh696@gmail.com>
2023-04-08 21:47:44 +0530

    Build interactive rating component

---
+++ index.js
@@ -0,0 +1,40 @@
+//const btn= document.getElementsByClassName("btn");
+
+function myFunction() {
+  let firstContainer = document.getElementById("firstContainer");
+  firstContainer.classList.add("element-hidden");
+  let secondContainer = document.getElementById("secondContainer");
+  secondContainer.classList.remove("element-hidden");
+}
+
+function mySecondFunction() {
+  let ratingContainer = document.getElementById("1");
+  let ratingNumber = document.getElementById("1N");
+  ratingContainer.style.backgroundColor = "hsl(216, 12%, 54%)";
+  ratingNumber.style.color = "white";
+}
+
+function myThirdFunction() {
+  let ratingContainer = document.getElementById("2");
+  let ratingNumber = document.getElementById("2N");
+  ratingContainer.style.backgroundColor = "hsl(216, 12%, 54%)";
+  ratingNumber.style.color = "white";
+}
+function myFourthFunction() {
+  let ratingContainer = document.getElementById("3");
+  let ratingNumber = document.getElementById("3N");
+  ratingContainer.style.backgroundColor = "hsl(216, 12%, 54%)";
+  ratingNumber.style.color = "white";
+}
+function myFifthFunction() {
+  let ratingContainer = document.getElementById("4");
+  let ratingNumber = document.getElementById("4N");
+  ratingContainer.style.backgroundColor = "hsl(216, 12%, 54%)";
+  ratingNumber.style.color = "white";
+}
+function mySixthFunction() {
+  let ratingContainer = document.getElementById("5");
+  let ratingNumber = document.getElementById("5N");
+  ratingContainer.style.backgroundColor = "hsl(216, 12%, 54%)";
+  ratingNumber.style.color = "white";
+}
